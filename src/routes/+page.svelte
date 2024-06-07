0cd962bdde5ea44d15cc8c0c03ead89b32d302df
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2024-06-07 09:58:50 +0200

    fix: change noble-black with primary in homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,41 +7,41 @@
 </svelte:head>
 
 <script lang="ts">
-  import { goto } from '$app/navigation';
+import { goto } from '$app/navigation'
 
-  const routes: Record<string, string> = {
-    '1': 'about',
-    '2': 'contacts',
-    // '3': 'projects',
-    // '4': 'blog',
-  };
+const routes: Record<string, string> = {
+  '1': 'about',
+  '2': 'contacts',
+  // '3': 'projects',
+  // '4': 'blog',
+}
 
-  const comingSoonRoutes = ['projects', 'blog'];
+const comingSoonRoutes = ['projects', 'blog']
 
-  $effect(() => {
-    const handleKeydown = (e: KeyboardEvent) => {
-      const route = routes[e.key];
-      if (route) goto(`/${route}`);
-    };
+$effect(() => {
+  const handleKeydown = (e: KeyboardEvent) => {
+    const route = routes[e.key]
+    if (route) goto(`/${route}`)
+  }
 
-    window.addEventListener('keydown', handleKeydown);
-    return () => window.removeEventListener('keydown', handleKeydown);
-  });
+  window.addEventListener('keydown', handleKeydown)
+  return () => window.removeEventListener('keydown', handleKeydown)
+})
 </script>
 
 <main class="min-h-dvh flex flex-col items-center h-full justify-center gap-4">
   <h1 class="text-4xl font-sans uppercase">Alessio Marchi</h1>
   <div class="flex flex-col items-center justify-center w-full max-w-xs px-4 rounded-lg overflow-hidden">
     {#each Object.entries(routes) as [key, route]}
-      <a tabindex="0" hreflang="en" href={`/${route}`} class="flex items-center justify-between w-full hover:bg-noble-black-600 focus:bg-noble-black-600 focus:ring-0 focus:outline-none transition-colors duration-100 h-10 px-2">
+      <a tabindex="0" hreflang="en" href={`/${route}`} class="flex items-center justify-between w-full hover:bg-primary-600 focus:bg-primary-600 focus:ring-0 focus:outline-none transition-colors duration-100 h-10 px-2">
         <span class="first-letter:uppercase">{route}</span>
-        <kbd class="text-noble-black-300 text-sm">({key})</kbd>
+        <kbd class="text-primary-300 text-sm">({key})</kbd>
       </a>
     {/each}
     {#each comingSoonRoutes as route}
       <div class="flex items-center justify-between w-full h-10 px-2 text-primary-300">
         <span class="first-letter:uppercase">{route}</span>
-        <kbd class="text-noble-black-300 text-sm">(soon)</kbd>
+        <kbd class="text-primary-300 text-sm">(soon)</kbd>
       </div>
     {/each}
   </div>
